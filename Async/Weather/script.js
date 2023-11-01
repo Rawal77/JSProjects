da5ede80e70ddfa047f36841fe62aa7cd235a1e7
Rawal <rawaldallakoti@gmail.com>
2023-11-01 22:45:05 +0545

    Project regarding weather

---
+++ Async/Weather/script.js
@@ -0,0 +1,35 @@
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const apiKey = "72a5bcad92778cdeb78a10aeb1e27d0c"
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+const weatherIcon = document.querySelector('.weather-icon')
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city +  `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector('.error').style.display = 'block'
+        document.querySelector('.weather').style.display = 'none'
+    }
+
+    let data = await response.json();
+    document.querySelector('.city').innerHTML = data.name
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp)  + "°C"
+    document.querySelector('.wind').innerHTML = data.wind.speed + "Km/hr"
+
+    if(data.weather[0].main == 'Clouds'){
+        weatherIcon.innerHTML = '☁️'
+    }else if(data.weather[0].main == 'Clear'){
+        weatherIcon.innerHTML = '🌞'
+    }else if(data.weather[0].main == 'Rain'){
+        weatherIcon.innerHTML = '☔'
+    }else if(data.weather[0].main == 'Drizzle'){
+        weatherIcon.innerHTML = '💦'
+    }else if(data.weather[0].main == 'Mist'){
+        weatherIcon.innerHTML = '🌁'
+    }
+}
+
+searchBtn.addEventListener('click',()=>{
+    checkWeather(searchBox.value)
+})
